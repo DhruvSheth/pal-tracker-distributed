e88a1c55729f6615e6c64de5c610a173a9e888c3
Michelle He <mhe@pivotal.io>
2019-04-04 11:15:23 -0700

    cloudNativeDeveloperDistributedSystemWithCircuitBreaker
    Signed-off-by: Dhruv Sheth <dsheth@pivotal.io>

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,18 +1,33 @@
 package io.pivotal.pal.tracker.timesheets;
 
-import org.springframework.web.client.RestOperations;
+    import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+    import java.util.Map;
+    import java.util.concurrent.ConcurrentHashMap;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+    import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
 
-    private final RestOperations restOperations;
-    private final String endpoint;
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final RestOperations restOperations;
+  private final String endpoint;
+  private final Map<Long, ProjectInfo> projectCache = new ConcurrentHashMap<>();
 
-    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations = restOperations;
-        this.endpoint = registrationServerEndpoint;
-    }
+  public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+    this.restOperations = restOperations;
+    this.endpoint = registrationServerEndpoint;
+  }
 
-    public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
-    }
+  @HystrixCommand(fallbackMethod = "getProjectFromCache")
+  public ProjectInfo getProject(long projectId) {
+    ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+    projectCache.put(projectId, project);
+    return project;
+  }
+
+  public ProjectInfo getProjectFromCache(long projectId) {
+    logger.info("\"Getting project with id {} from cache\", projectId");
+    return projectCache.get(projectId);
+  }
 }
